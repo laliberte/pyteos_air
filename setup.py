d3aed751928874ca0a76f247c51ae91608282e7f
laliberte <laliberte.frederic@gmail.com>
2013-01-14 15:58:32 -0500

    Changed version number

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ packages=[package_name+'.'+name for name in find_packages('lib')]
 packages.append(package_name)
 
 setup(name=package_name,
-    version = "0.4",
+    version = "0.4.1",
     packages=packages,
     package_dir = {package_name: 'lib'},
 
