fce0acc75e19049f5595e8dd7881cd5ade046cfd
laliberte <laliberte.frederic@gmail.com>
2013-01-14 10:53:53 -0500

    Improved netcdf_interface

--- lib/pyteos_interface/netcdf_interface.py
+++ lib/pyteos_interface/netcdf_interface.py
@@ -4,14 +4,9 @@ import numpy as np
 import pickle
 import scipy.interpolate as interp
 
-def compute_both_theta(args):
+def compute_g(args):
     #LOAD THE DATA:
-    thermo = Dataset(args.in_thermodynamic_file)
-    thermo_data=dict()
-    dim_list={'hur':[len(thermo.dimensions['hur']),1,1],
-                'ta':[1,len(thermo.dimensions['ta']),1],
-                'pa':[1,1,len(thermo.dimensions['pa'])]}
-
+    thermo = pickle.load(args.in_thermodynamic_file)
     data = Dataset(args.in_netcdf_file)
 
     #CREATE THE OUTPUT FILE
@@ -35,60 +30,26 @@ def compute_both_theta(args):
         dim_var[:] = data.variables[dims][:]
         output = replicate_netcdf_var(output,data,dims)
 
-    out_var_list=thermo.variables.keys()
-    if not set(['rh_wmo','a_sat']).issubset(out_var_list):
-        raise IOError('Variable rh_wmo and a_sat should be in input thermodynamical file')
-    for var in out_var_list:
-        thermo_data[var]=thermo.variables[var][:]
-        if var in dim_list.keys():
-            thermo_data[var]=np.reshape(thermo_data[var],dim_list[var])
-        else:
-            output.createVariable(var,'f',data.variables['ta'].dimensions)
-    #Deal with rh_wmo explicitly:
-    out_var_list.remove('rh_wmo')
-    out_var_list.remove('a_sat')
-    #Remove dimensions:
-    out_var_list=[ x for x in out_var_list if (x not in dim_list)]
-        
-    interp={}
-    interp['a_sat']=interp_linear(thermo_data['a_sat'],thermo_data['ta'],thermo_data['pa'])
-    for var in out_var_list:
-        print var
-        interp['var']=interp_linear(thermo_data[var],
-             thermo_data['hur'],
-             thermo_data['ta'],
-             thermo_data['pa']
-             )
+    out_var_list=thermo.keys()
+    if not 'massfraction_air' in out_var_list:
+        raise IOError('Variable massfraction_air should be in input thermodynamical file')
 
     time_length=len(data.dimensions['time']) 
     for t_id in range(0,time_length):
         print(t_id)
-        a  =1.0-data.variables['hus'][t_id,...]
-        ta =data.variables['ta'][t_id,...]
-        pa =data.variables['pa'][t_id,...]
+        A  =1.0-data.variables['hus'][t_id,...]
+        T =data.variables['ta'][t_id,...]
+        p =data.variables['pa'][t_id,...]
 
-        ax_ind=np.argmax(ta.shape)
+        massfraction_air=thermo['massfraction_air'](T,p)
         #First find the relative humidity:
-        a_sat=np.reshape(a_sat['a_sat'](coord_trans(ta,pa)),ta.shape)
-        hur=(1.0 / a - 1.0) / (1.0 / a_sat - 1.0)
+        hur=(1.0 / A - 1.0) / (1.0 / massfraction_air - 1.0)
         output.variables['rh_wmo'][t_id,...]=hur
         for var in out_var_list:
-            data.variables[var][t_id,...]=np.reshape(
-                                                     interp[var](coors_trans(hur,ta,pa)),
-                                                     ta.shape
-                                                     )
+            data.variables[var][t_id,...]=thermo[var](A,T,p)
         output.sync()
 
     output.close()
-
-def interp_linear(nd_array,*args):
-    nd_interp=interp.LinearNDInterpolator(
-                            coord_trans(*args),
-                            np.ravel(nd_array))
-    return nd_interp
-
-def coord_trans(*arr):
-    return np.vstack(map(lambda x: np.ravel(x),np.broadcast_arrays(*arr))).T
 
 def replicate_netcdf_file(output,data):
 	for att in data.ncattrs():
@@ -145,8 +106,8 @@ if __name__ == "__main__":
                             version='%(prog)s '+version_num,
                             epilog=epilog)
 
-    parser.add_argument('in_thermodynamic_file',
-                         help='Input netCDF file with variables pottemp,potempequi, rh_wmo on a rh_wmo, ta, pa grid')
+    parser.add_argument('in_thermodynamic_file',type=argparse.FileType('r'),
+                         help='Input netCDF file with variables pottemp,potempequi, massfraction_air on a rh_wmo, ta, pa grid')
     parser.add_argument('in_netcdf_file',
                          help='Input netCDF file with variables ta, pa, hus')
     parser.add_argument('out_netcdf_file',
@@ -157,5 +118,5 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    compute_both_theta(args)
+    compute_g(args)
 
