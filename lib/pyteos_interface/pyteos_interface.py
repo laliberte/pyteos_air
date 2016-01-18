b0edf8aa719428d101cf514e2e1e647d6c062f11
laliberte <laliberte.frederic@gmail.com>
2016-01-18 08:42:55 -0500

    Found a bug arising from newer f2py.

--- lib/pyteos_interface/pyteos_interface.py
+++ lib/pyteos_interface/pyteos_interface.py
@@ -3,5 +3,9 @@ import inspect
 import pyteos_air.teos_air as teos_air
 
 def attribute_to_function(method_name,function_name,*args):
+    #in_args=np.broadcast_arrays(*np.atleast_3d(*args))
+    #in_args+=(np.empty_like(in_args[0]),)
+    #getattr(teos_air,method_name)(function_name,*in_args)
+    #return np.squeeze(in_args[-1])
     return np.squeeze(getattr(teos_air,method_name)(function_name,*np.broadcast_arrays(*np.atleast_3d(*args))))
 
