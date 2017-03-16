ace27436bbdfc88ec22981d304dc66f979812f85
laliberte <laliberte.frederic@gmail.com>
2017-03-16 09:36:29 -0400

    Minimal testing and clean-up

--- pyteos_air/tests/test_liq_ice_air.py
+++ pyteos_air/tests/test_liq_ice_air.py
@@ -9,9 +9,18 @@ from pyteos_air import liq_ice_air
 
 
 def test_g_entropy():
-    # From src/SIA_library/Air_3b.F90
-    # assert liq_ice_air.g.entropy(0.9, 300.0, 1e5) == 911.170080461
-    # From src/SIA_library/Ice_Air_4b.F90
-    assert liq_ice_air.g.entropy(0.5, 270.0, 1e5) == -610.264515318
-    # From src/SIA_library/Liq_Air_4b.F90
-    assert liq_ice_air.g.entropy(0.5, 300.0, 1e5) == 343.783393872
+    # Below freezing:
+    assert liq_ice_air.g.entropy(0.98, 270.0, 1e5) == 167.3697059091392
+    # Above freezing:
+    assert liq_ice_air.g.entropy(0.98, 300.0, 1e5) == 276.53730016658955
+    # Low pressure, must be dry:
+    assert liq_ice_air.g.entropy(1.0, 300.0, 5e3) == 958.6649347968464
+
+
+def test_g_ref_pottemp():
+    # Below freezing:
+    assert liq_ice_air.g_ref.pottemp(0.98, 270.0, 5e4, 1e5) == 328.77732775760836
+    # Above freezing:
+    assert liq_ice_air.g_ref.pottemp(0.98, 300.0, 5e4, 1e5) == 365.2154567609769
+    # Low pressure, must be dry:
+    assert liq_ice_air.g_ref.pottemp(1.0, 300.0, 5e3, 1e5) == 692.815889987961
